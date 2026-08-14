6d6d4e8d8753f5a6d9ab3e1e54df78dfb9554cb7
Altarok <thom.hammerklinge@gmail.com>
2026-08-14 17:00:41 +0200

    make gantt-item optional during event collecting

---
+++ src/io/file-collector.ts
@@ -0,0 +1,38 @@
+import {TFile} from 'obsidian'
+import FantasyGanttPlugin from '../main'
+import {CodeBlockContent, PluginSettings} from '../const/types'
+import {Consts} from '../const/constants'
+import {FrontMatterUtil} from "./frontmatter-reader";
+
+export function getFilteredFiles(plugin: FantasyGanttPlugin,
+                                 pluginSettings: PluginSettings,
+                                 codeBlockContent: CodeBlockContent): TFile[] {
+  const allFiles = plugin.app.vault.getMarkdownFiles()
+
+  let eventSourcePath = codeBlockContent.eventPath ?? pluginSettings.eventPath
+  /* Normalize root path reference */
+  if (eventSourcePath === Consts.ROOT_PATH) eventSourcePath = Consts.ROOT_PATH_NORMALIZED
+
+  const filesInCorrectPath = allFiles.filter(f => {
+    const parentPath = f.parent?.path ?? ''
+
+    if (codeBlockContent.eventPathSearchRecursive ?? pluginSettings.eventPathSearchRecursive)
+      return eventSourcePath === '' || parentPath === eventSourcePath || parentPath.startsWith(eventSourcePath + Consts.DIR_SEPARATOR)
+    else
+      return parentPath === eventSourcePath
+  })
+
+  const {frontMatterProperty_gantt_this_optional} = plugin.settings
+
+  return filesInCorrectPath.filter(f => {
+    const cache = plugin.app.metadataCache.getFileCache(f)
+    const frontMatter = cache?.frontmatter
+
+    if (!frontMatter) return false
+
+    if (frontMatterProperty_gantt_this_optional)
+      return true
+    else
+      return FrontMatterUtil.isFileRelevant(frontMatter, plugin.settings)
+  })
+}
