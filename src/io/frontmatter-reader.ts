538279be9b7e1ab6c00e502105399ce29f5ce13f
Altarok <thom.hammerklinge@gmail.com>
2026-08-15 22:13:19 +0100

    eslint: remove redundant assertion

--- src/io/frontmatter-reader.ts
+++ src/io/frontmatter-reader.ts
@@ -68,9 +68,9 @@ function getEventSymbol(frontMatter: FrontMatterCache, settings: PluginSettings,
 //  if (value && !isGanttItemDisplayType(value)) value = undefined
 
   if (isTimeSpan) {
-    return GanttItemDisplayTypes.isTimespan(value) ? value : settings.uxDefaultTimespanEventSymbol as GanttItemDisplayType
+    return GanttItemDisplayTypes.isTimespan(value) ? value : settings.uxDefaultTimespanEventSymbol
   } else {
-    return GanttItemDisplayTypes.isTimestamp(value) ? value : settings.uxDefaultTimestampEventSymbol as GanttItemDisplayType
+    return GanttItemDisplayTypes.isTimestamp(value) ? value : settings.uxDefaultTimestampEventSymbol
   }
 }
 
