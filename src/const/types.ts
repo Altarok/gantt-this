d8dd2cf881d19e9d25c7bd55362ae56bdd2c6f2e
Altarok <thom.hammerklinge@gmail.com>
2026-08-19 09:53:42 +0200

    improve naming of default calendar propertys

--- src/const/types.ts
+++ src/const/types.ts
@@ -269,11 +269,11 @@ export const DEFAULT_SETTINGS: PluginSettings = {
   /*
    * Front-matter property names
    */
-  frontMatterProperty_calendar_name: 'gantt-type-definition', // string, activates file as calendar source
+  frontMatterProperty_calendar_name: 'gantt-calendar-definition', // string, activates file as calendar source
 
   frontMatterProperty_gantt_this: 'gantt-item', // boolean, activates file as event source
   frontMatterProperty_gantt_this_optional: true,
-  frontMatterProperty_event_calendar: 'gantt-type', // name of matching calendar or 'gregorian' TODO rename to gantt-calendar
+  frontMatterProperty_event_calendar: 'gantt-calendar', // name of matching calendar or 'gregorian' TODO rename to gantt-calendar
   frontMatterProperty_event_time_start: 'gantt-start', // start of event (or timestamp if no end is given )
   frontMatterProperty_event_time_end: 'gantt-end', // ... or time.start
   frontMatterProperty_event_name: 'gantt-name', // ... or filename
